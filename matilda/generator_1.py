2057cd24a569d3296fe163281203d23d4572a868
Kunal Mehta <legoktm@gmail.com>
2013-04-10 15:17:01 -0500

    Flatten directory structure

--- matilda/generator_1.py
+++ matilda/generator_1.py
@@ -20,6 +20,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 """
+import cgitb
+cgitb.enable()
 import matilda
 from matilda import html
 from matilda import config
