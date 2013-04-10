c4c778db5c396bbca5b954d41f16a2d0fb6e1a0f
Kunal Mehta <legoktm@gmail.com>
2013-04-10 15:21:14 -0500

    Add ct()

--- main.py
+++ main.py
@@ -20,9 +20,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 """
-import matilda
+import cgitb
+cgitb.enable()
 generators = dict()
 
 
 def addGenerator(name, cls):
     generators[name] = cls
+
+
+def ct():
+    return "Content-type: text/html\n\n"
