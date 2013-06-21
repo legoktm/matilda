c15e51430eb619af14a635dc1f626dff0595f313
Kunal Mehta <legoktm@gmail.com>
2013-06-21 00:39:04 -0700

    add unittests for Job and Log classes

--- tests/main.py
+++ tests/main.py
@@ -24,12 +24,16 @@ IN THE SOFTWARE.
 
 import unittest
 
+import datetime
 import os
 import simplejson
 from matilda import matilda
 
 
 class TestBot(unittest.TestCase):
+
+    def setUp(self):
+        self.line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|create=yes|recursion=2}}"
 
     def test_dir_creation(self):
         matilda.create_archive_dir()
@@ -44,6 +48,22 @@ class TestBot(unittest.TestCase):
             data = simplejson.load(f)
         self.assertTrue('updated' in data)
         self.assertTrue('jobs' in data)
+
+    def testJob(self):
+        j = matilda.Job(self.line)
+        self.assertEqual(j.lang, 'fr')
+        self.assertEqual(j.data['claims'], [(u'P107', u'Q618123'), (u'P31', u'Q10387575'), (u'P17', u'Q142')])
+        self.assertEqual(j.source, 'Category:Monument historique inscrit')
+        self.assertTrue(j.create)
+
+    def testLog(self):
+        j = matilda.Job(self.line)
+        l = matilda.Log(j)
+        self.assertTrue(l.start < datetime.datetime.today())
+        self.assertEqual(l.data, [])
+        l.append('test')
+        self.assertEqual(l.data, ['test'])
+
 
 
 if __name__ == "__main__":
