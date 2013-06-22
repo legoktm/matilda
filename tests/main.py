4aba7689d2f1b012fbb008fee99233fc06bf1751
Kunal Mehta <legoktm@gmail.com>
2013-06-22 12:05:07 -0700

    split tests into more functions

--- tests/main.py
+++ tests/main.py
@@ -32,9 +32,6 @@ from matilda import matilda
 
 class TestBot(unittest.TestCase):
 
-    def setUp(self):
-        self.line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|nocreate=yes|recursion=2}}"
-
     def test_dir_creation(self):
         matilda.create_archive_dir()
         self.assertTrue(os.path.exists(os.path.expanduser('~/public_html/archives')))
@@ -49,12 +46,28 @@ class TestBot(unittest.TestCase):
         self.assertTrue('updated' in data)
         self.assertTrue('jobs' in data)
 
+    def testCreate(self):
+        line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|nocreate=yes|recursion=2}}"
+        j = matilda.Job(line)
+        self.assertFalse(j.create)
+        line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|create=yes|recursion=2}}"
+        j = matilda.Job(line)
+        self.assertTrue(j.create)
+
+    def testRecursion(self):
+        line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|nocreate=yes|recursion=2}}"
+        j = matilda.Job(line)
+        self.assertEqual(j.recursion, 2)
+        line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|nocreate=yes}}"
+        j = matilda.Job(line)
+        self.assertEqual(j.recursion, 0)
+
     def testJob(self):
-        j = matilda.Job(self.line)
+        line = "{{User:Legobot/properties.js/row|fr|Category:Monument historique inscrit|P107|Q618123|pid2=P31|qid2=Q10387575|pid3=P17|qid3=Q142|Ayack|nocreate=yes|recursion=2}}"
+        j = matilda.Job(line)
         self.assertEqual(j.lang, 'fr')
         self.assertEqual(j.data['claims'], [(u'P107', u'Q618123'), (u'P31', u'Q10387575'), (u'P17', u'Q142')])
         self.assertEqual(j.source, 'Category:Monument historique inscrit')
-        self.assertFalse(j.create)
         self.assertEqual(j.data['user'], 'Ayack')
         self.assertLess(j._start, datetime.datetime.today())
 
